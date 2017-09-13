288f6be81f3f0b9c94c193025f1a77f14981f1ac
maxcheremisin <maxcheremisin2008@gmail.com>
2017-09-14 02:02:29 +0300

    event-delegation

--- script.js
+++ script.js
@@ -1,71 +1,11 @@
 (function () {
     'use strict';
 
-    function uncheckedRadio() {
-        let allRadios = document.getElementsByName('menu-button');
-        let radio;
-        let x;
-
-        for (x = 0; x < allRadios.length; x++) {
-            allRadios[x].onclick = function () {
-                if (radio === this) {
-                    this.checked = false;
-                    radio = null;
-                } else {
-                    radio = this;
-                }
-            };
-        }
-    }
-
-    function dropDownMenu(selector) {
-        document.querySelector(selector).addEventListener('click', (function () {
-            let input = document.querySelector(selector);
-            let menu = document.querySelector('.js-drop-menu');
-
-            input.checked ? menu.style.display = 'flex' : menu.style.display = '';
-        }).bind(null, selector));
-    }
-
-    function showArticle(selector) {
-        document.querySelector(selector).addEventListener('click', (function () {
-            let input = document.querySelector(selector);
-            let inputClasses = input.classList;
-            let buttonName = inputClasses[inputClasses.length - 1].split('--')[1];
-            let button = document.querySelector('.js-radio-button--' + buttonName);
-            let buttonClasses = button.classList;
-            let targetClass = buttonClasses[buttonClasses.length - 1];
-            let article = document.querySelector('.js-article--' + buttonName);
-            let articles = document.getElementsByClassName('js-article');
-
-            if (targetClass === 'radio-button--checked') {
-                button.classList.remove('radio-button--checked');
-                for (let i = 0; i < articles.length; i++) {
-                    articles[i].style.display = '';
-                }
-            } else if (document.querySelector('.radio-button--checked') !== null) {
-                document.querySelector('.radio-button--checked').classList.remove('radio-button--checked');
-                button.classList.add('radio-button--checked');
-                for (let i = 0; i < articles.length; i++) {
-                    articles[i].style.display = '';
-                }
-                article.style.display = 'flex';
-            } else {
-                button.classList.add('radio-button--checked');
-                for (let i = 0; i < articles.length; i++) {
-                    articles[i].style.display = '';
-                }
-                article.style.display = 'flex';
-            }
-
-        }).bind(null, selector));
-    }
-
     function onScroll() {
         let width = window.innerWidth;
         let header = document.querySelector('.js-header-onscroll');
         let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-        let logoHeight = width <= 473 ? 55 : 87;
+        let logoHeight = width <= 523 ? 55 : 87;
 
         if (scrolled >= logoHeight) {
             header.style.position = 'sticky';
@@ -75,13 +15,79 @@
         }
     }
 
+    function showArticle(event) {
+        let target = event.target;
+        let targetClass;
+        let buttonName;
+        let article;
+        let articles;
+
+        while (target !== this) {
+            if (target.matches('.js-button')) {
+
+                targetClass = target.classList[target.classList.length - 1];
+
+                buttonName = targetClass.split('--')[1] === 'checked' ?
+                    target.classList[target.classList.length - 2].split('--')[1] :
+                    target.classList[target.classList.length - 1].split('--')[1];
+
+                article = document.querySelector('.js-article--' + buttonName);
+                articles = document.getElementsByClassName('js-article');
+
+                if (buttonName === 'hide') {
+                    document.querySelector('.radio-button--checked').classList.remove('radio-button--checked');
+                    for (let i = 0; i < articles.length; i++) {
+                        articles[i].style.display = '';
+                    }
+                } else if (targetClass === 'radio-button--checked') {
+                    target.classList.remove('radio-button--checked');
+                    for (let i = 0; i < articles.length; i++) {
+                        articles[i].style.display = '';
+                    }
+                } else if (document.querySelector('.radio-button--checked') !== null) {
+                    document.querySelector('.radio-button--checked').classList.remove('radio-button--checked');
+                    target.classList.add('radio-button--checked');
+                    for (let i = 0; i < articles.length; i++) {
+                        articles[i].style.display = '';
+                    }
+                    article.style.display = 'flex';
+                } else {
+                    target.classList.add('radio-button--checked');
+                    for (let i = 0; i < articles.length; i++) {
+                        articles[i].style.display = '';
+                    }
+                    article.style.display = 'flex';
+                }
+
+                console.log(targetClass);
+                event.preventDefault();
+                return;
+            } else if (target.tagName === 'BODY') {
+                return;
+            }
+            target = target.parentNode;
+        }
+    }
+
+    function dropDownMenu() {
+        let target = event.target;
+        let menu = document.querySelector('.js-drop-menu');
+
+        while (target !== this) {
+            if (target.matches('.js-burger')) {
+                target.checked ? menu.style.display = 'flex' : menu.style.display = '';
+                return;
+            } else if (target.tagName === 'BODY') {
+                return;
+            } else if (!target.closest('.menu')) {
+                menu.style.display = '';
+                document.querySelector('.js-burger').checked = false;
+            }
+            target = target.parentNode;
+        }
+    }
+
     window.onscroll = onScroll;
-    uncheckedRadio();
-    showArticle('.js-button--about');
-    showArticle('.js-button--information');
-    showArticle('.js-button--tags');
-    showArticle('.js-hide-button--about');
-    showArticle('.js-hide-button--information');
-    showArticle('.js-hide-button--tags');
-    dropDownMenu('.js-button--burger');
+    window.addEventListener('click', showArticle);
+    window.addEventListener('click', dropDownMenu)
 }());
